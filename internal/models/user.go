9148acea5f524c1158333aa9d36080d0eb8a3dfb
elviscgn <96030189+elviscgn@users.noreply.github.com>
2025-12-21 07:20:10 +0200

    standardize user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,10 @@
 package models
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Picture  string `json:"picture"`
+	GoogleID string `json:"google_id"`
+	Role     string `json:"role"`
+}
